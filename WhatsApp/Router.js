db4a84942ecac491d33ed3ad8a4308ea45e13b71
akdenizburak <akdenizburakk7@gmail.com>
2022-08-25 02:56:10 +0300

    Edited Router.js

--- WhatsApp/Router.js
+++ WhatsApp/Router.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import { View,Text} from '@react-navigation/native'
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomePage from './src/pages/HomePage';
-import Chat from './src/pages/Chat'
+import ChatPage from './src/pages/ChatPage'
 
 const Stack=createNativeStackNavigator();
 
@@ -12,7 +11,7 @@ function App(){
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name='HomePage'component={HomePage} options={{headerShown: false}}/>
-        <Stack.Screen name='Chat' component={Chat} options={{headerShown: false}}/>
+        <Stack.Screen name='ChatPage' component={ChatPage} options={{headerShown: false}}/>
       </Stack.Navigator>
     </NavigationContainer>
   )
